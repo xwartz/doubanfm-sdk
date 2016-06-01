abfafcd5e98df1c841c468ec3fdb7edb28e3c8c6
xwartz <stddup@gmail.com>
2016-06-01 15:37:06 +0800

    test songs

--- test/song_spec.js
+++ test/song_spec.js
@@ -13,4 +13,35 @@ describe('#songs', () => {
       })
     })
   })
+
+  describe('#star()', () => {
+    it('it should return an array', (done) => {
+      sdk.star({ channel: '1', sid: '1395259' }, (err, data) => {
+        if (err) throw err
+        expect(data).to.be.a('array')
+        done()
+      })
+    })
+  })
+
+  describe('#unstar()', () => {
+    it('it should return an array', (done) => {
+      sdk.unstar({ channel: '1', sid: '1395259' }, (err, data) => {
+        if (err) throw err
+        expect(data).to.be.a('array')
+        done()
+      })
+    })
+  })
+
+  describe('#never_play_again()', () => {
+    it('it should return an array', (done) => {
+      sdk.never_play_again({ channel: '1', sid: '1395259' }, (err, data) => {
+        if (err) throw err
+        expect(data).to.be.a('array')
+        done()
+      })
+    })
+  })
+
 })
